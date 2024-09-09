ba6f40f3514b74f1ca34949f9ca737676cc7ad9e
David Mkrtchyan <david.mkrtchyan@gmail.com>
2024-09-09 18:36:52 +0400

    CReate empty project

---
+++ src/main/java/com/cloudchipr/workflow/WorkflowEgineApplication.java
@@ -0,0 +1,13 @@
+package com.cloudchipr.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowEgineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowEgineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudchipr/workflow/WorkflowEgineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudchipr.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowEgineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
